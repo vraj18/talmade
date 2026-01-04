b93f6d30d3a82558e37d8b06e56bc1c2b21218b6
vraj18 <vrajborad18@gmail.com>
2026-01-04 15:45:03 +0530

    updated App

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ import { ProductDetail } from './pages/ProductDetail';
 // import { ProductDetail } from './pages/ProductDetail';
 import { CategoryPage } from './pages/CategoryPage';
 import { ClientsPage } from './pages/ClientsPage';
-import { BlogPage } from './pages/BlogPage';
 import { EnquiryPage } from './pages/EnquiryPage';
 import { AboutPage } from './pages/AboutPage';
 import { ContactPage } from './pages/ContactPage';
@@ -21,7 +20,6 @@ function App() {
           <Route path="products/:id" element={<ProductDetail />} />
           <Route path="category/:id" element={<CategoryPage />} />
           <Route path="clients" element={<ClientsPage />} />
-          <Route path="blog" element={<BlogPage />} />
           <Route path="enquiry" element={<EnquiryPage />} />
           <Route path="contact-us" element={<ContactPage />} />
         </Route>
